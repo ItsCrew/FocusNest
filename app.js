088409317ab3da8ccff1813edf8f1668592f8f41
ItsCrew <aryanchanana20@gmail.com>
2025-05-14 02:33:08 +0530

    Styling Changes
    Added Icons will add more tomorrow. also did some styling changes with the mainpagecontent

---
+++ app.js
@@ -0,0 +1,6 @@
+// const bars = document.querySelector(".bars")
+// const SideBar = document.querySelector(".SideBar")
+
+// bars.addEventListener("click", () => {
+//     SideBar.style.display = "none"
+// })
