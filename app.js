6ddad6bea3615563ccdaf1371ccc3433d7a55f0f
ItsCrew <aryanchanana20@gmail.com>
2025-05-15 03:37:09 +0530

    Added Dark Mode
    Dark Mode logic and styling done just have to decide the colour for dark mode and i have some stuff in mind but i am going to do that later

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 // const bars = document.querySelector(".bars")
-// const SideBar = document.querySelector(".SideBar")
+const SideBar = document.querySelector(".SideBar")
+const DarkModeDiv = document.querySelector(".DarkModeDiv")
+const DarkMode = document.querySelector(".DarkMode")
+const LightModeDiv = document.querySelector(".LightModeDiv")
+const LightMode = document.querySelector(".LightMode")
 
 // bars.addEventListener("click", () => {
 //     SideBar.style.display = "none"
 // })
+
+// Dark and Light mode logic
+DarkMode.addEventListener("click", () => {
+    LightModeDiv.style.display = "block"
+    DarkModeDiv.style.display = "none"
+    document.body.style.backgroundColor = "#1e2d24"
+    document.body.style.color = "#ededed"
+    SideBar.style.backgroundColor = "#2d3c33"
+})
+
+LightMode.addEventListener("click", () => {
+    LightModeDiv.style.display = "none"
+    DarkModeDiv.style.display = "block"
+    document.body.style.backgroundColor = "#f7f7f7"
+    document.body.style.color = "#333333"
+    SideBar.style.backgroundColor = "#91c788"
+
+})
