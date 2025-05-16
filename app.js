389159f7c82f07fd171c8a05560011a5f04d4afa
ItsCrew <aryanchanana20@gmail.com>
2025-05-16 20:17:48 +0530

    Style Changed and Styling and functioning of Side Bar Options
    Side Bar Options are not styled and They are also functional even though there is nothing on those pages yet. Some minor Styling Changes on the Home Page aswell.

--- app.js
+++ app.js
@@ -4,11 +4,32 @@ const DarkModeDiv = document.querySelector(".DarkModeDiv")
 const DarkMode = document.querySelector(".DarkMode")
 const LightModeDiv = document.querySelector(".LightModeDiv")
 const LightMode = document.querySelector(".LightMode")
+const PomodoroMode = document.querySelector(".PomodoroMode")
+const ShortBreak = document.querySelector(".ShortBreak")
+const LongBreak = document.querySelector(".LongBreak")
+const Timer = document.querySelector(".Timer")
+const ModeButton = document.querySelectorAll(".ModeButton")
+const Navigation = document.querySelector(".Navigation")
+
 
 bars.addEventListener("click", () => {
     SideBar.classList.toggle("hide");
     bars.classList.toggle("shift-left");
 });
+
+const ModesMap = {
+    PomodoroMode: "Default",
+    ShortBreak: "Short Break",
+    LongBreak: "Long Break",
+}
+
+ModeButton.forEach(button => {
+    button.addEventListener("click", () => {
+        const key = button.classList[0];
+        Timer.textContent = ModesMap[key];
+    })
+});
+
 
 // Dark and Light mode logic
 DarkMode.addEventListener("click", () => {
@@ -17,6 +38,7 @@ DarkMode.addEventListener("click", () => {
     document.body.style.backgroundColor = "#1e2d24"
     document.body.style.color = "#ededed"
     SideBar.style.backgroundColor = "#2d3c33"
+    // Navigation.style.backgroundColor = "#6b8f71"
 })
 
 LightMode.addEventListener("click", () => {
@@ -25,5 +47,8 @@ LightMode.addEventListener("click", () => {
     document.body.style.backgroundColor = "#f7f7f7"
     document.body.style.color = "#333333"
     SideBar.style.backgroundColor = "#91c788"
+    // Navigation.style.backgroundColor = "#94bd9b"
+
 
 })
+
