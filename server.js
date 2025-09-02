4d82ff722a7c2c5192529d77e56b7f354a958ba7
ItsCrew <aryanchanana20@gmail.com>
2025-09-02 20:48:43 +0530

    Setup express server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+app.use(express.static('./public'))
+app.use(express.json())
+
+const port = process.env.PORT
+
+const start = async () => {
+    try {
+        await app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}...`);
+
+        })
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+start()
