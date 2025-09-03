d868cfd6e3199edd3fe5da3743e6898829c4cda3
ItsCrew <aryanchanana20@gmail.com>
2025-09-03 23:10:51 +0530

    Databse Setup and Redirect to Home page
    The redirect will be changed in future to About page

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
+const connectDB = require('./Database/connect')
 
 dotenv.config()
 
 app.use(express.static('./public'))
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.redirect('/Home.html')
+})
+
 const port = process.env.PORT
 
 const start = async () => {
     try {
-        await app.listen(port, () => {
+        await connectDB(process.env.MONGO_URI)
+        console.log('Connected to the Database');
+
+        app.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}...`);
 
         })
