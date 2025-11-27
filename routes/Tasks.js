47b3accf2d12b59140ca6c1724655c673eb9d764
ItsCrew <aryanchanana20@gmail.com>
2025-11-27 17:33:40 +0530

    Updated Task Routes

--- routes/Tasks.js
+++ routes/Tasks.js
@@ -4,7 +4,8 @@ const { isAuthenticated } = require('../Middleware/Auth')
 
 const { FetchAllTasks, CreateTask, DeleteTask, ClearAll, EditTask, ReorderTasks, FetchPriority } = require('../Controllers/Tasks')
 
-router.route('/').get(isAuthenticated, FetchPriority).post(isAuthenticated, CreateTask).delete(isAuthenticated, ClearAll)
+router.route('/').get(isAuthenticated, FetchAllTasks).post(isAuthenticated, CreateTask).delete(isAuthenticated, ClearAll)
 router.route('/reorder').post(isAuthenticated, ReorderTasks)
+router.route('/priorities').get(isAuthenticated, FetchPriority)
 router.route('/:id').delete(isAuthenticated, DeleteTask).patch(isAuthenticated, EditTask)
 module.exports = router;
